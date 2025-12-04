3a76bf2275968adc4413d0ad558cc978ef70f33f
cristinamerisoiu <44377572+cristinamerisoiu@users.noreply.github.com>
2025-12-04 15:29:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ new Chart(categoriesCtx, {
     options: {
         indexAxis: 'y',
         responsive: true,
-        maintainAspectRatio: false,
+        maintainAspectRatio: true,
         scales: {
             x: {
                 beginAtZero: true,
